a1ff303cd743bca4de0ede6f3f37d70fa228d6da
Ryuta Hamasaki <hamasaki@kurashicom.jp>
2016-09-13 10:26:33 +0900

    implement gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var sh = require('shelljs');
+var jslibs = [
+
+];
+
+gulp.task('default', [
+    'watch-sass',
+    'watch-concat-js-main',
+    'watch-uglify-js-main',
+    'watch-concat-js-lib',
+    'watch-uglify-js-lib'
+]);
+
+// exec tasks
+gulp.task('sass', function(done) {
+    gulp.src('scss/app.scss')
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('dist/css'))
+        .on('end', done);
+});
+
+gulp.task('concat-js-main', function(done) {
+    gulp.src(['js/**/*.js'])
+        .pipe(concat('main.js', {newLine: '\n\n\n'}))
+        .pipe(gulp.dest('dist/js'))
+        .on('end', done);
+});
+
+gulp.task('uglify-js-main', function(done) {
+    gulp.src('dist/js/main.js')
+        .pipe(sourcemaps.init())
+            .pipe(uglify())
+            .pipe(concat('main.min.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/js'))
+        .on('end', done);
+});
+
+gulp.task('concat-js-lib', function(done) {
+    gulp.src(jslibs)
+        .pipe(concat('dependencies.js', {newLine: '\n\n\n'}))
+        .pipe(gulp.dest('dist/js'))
+        .on('end', done);
+});
+
+gulp.task('uglify-js-lib', function(done) {
+    gulp.src('dist/js/dependencies.js')
+        .pipe(sourcemaps.init())
+            .pipe(uglify())
+            .pipe(concat('dependencies.min.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/js'))
+        .on('end', done);
+});
+
+// watch tasks
+gulp.task('watch-sass', function() {
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('watch-concat-js-main', function() {
+    gulp.watch('js/**/*.js', ['concat-js-main']);
+});
+
+gulp.task('watch-uglify-js-main', function() {
+    gulp.watch('dist/js/main.js', ['uglify-js-main']);
+});
+
+gulp.task('watch-concat-js-lib', function() {
+    gulp.watch(jslibs, ['concat-js-lib']);
+});
+
+gulp.task('watch-uglify-js-lib', function() {
+    gulp.watch('dist/js/dependencies.js', ['uglify-js-lib']);
+});
